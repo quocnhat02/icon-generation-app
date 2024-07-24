7c6d721139648f73393e70361ac643970e5f6b1b
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-24 19:26:15 +0700

    update credits

--- src/server/api/routers/generate.ts
+++ src/server/api/routers/generate.ts
@@ -1,16 +1,45 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 export const generateRouter = createTRPCRouter({
-  generateIcon: publicProcedure
+  generateIcon: protectedProcedure
     .input(
       z.object({
         prompt: z.string(),
       })
     )
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
       console.log("we are here", input.prompt);
+
+      // TODO: verify the user has enough credits
+      const { count } = await ctx.prisma.user.updateMany({
+        where: {
+          id: ctx.session.user.id, // TODO: replace with a real id
+          credits: {
+            gte: 1,
+          },
+        },
+        data: {
+          credits: {
+            decrement: 1,
+          },
+        },
+      });
+
+      if (count <= 0) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "you do not have enough credits",
+        });
+      }
+
+      //   TODO: submit prompt
 
       return {
         message: "success",
