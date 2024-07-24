b91e9df2c813a4c1bfcbc267f9f952c32f980eca
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-07-24 16:22:25 +0700

    generate page

---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+function Input(props: React.ComponentPropsWithoutRef<"input">) {
+  return (
+    <input {...props} type="text" className="border border-gray-800"></input>
+  );
+}
+
+export default Input;
